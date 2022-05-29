4d02cf88f4aba616f7149615e9e1b6f1e889f986
Brittany <brittanylcallus@gmail.com>
2022-05-28 23:47:14 -0400

    generateRandomApple added

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 //Using How to Build a Snake Game In JavaScript from freeCodeCamp.org as a guide to help me through getting the game functioning. I am not just copying the work, I am working through all of its parts so that I have an understanding of what each part does. I am changing things when I am confident that I know of another way to achieve the same outcome. 
 
-let gameBoard = document.getElementById("game-board");
-let scoreDisplay = document.getElementsByClassName("score-display");
+const gameBoard = document.getElementById("game-board");
+const scoreDisplay = document.getElementsByClassName("score-display");
 let snake = [gameBoard[0]];
 let score = 0;
 let intervalTime = 0;
 let interval = 0;
 let direction = 1; //adding one means to the right
 let width = 20; //maybe change this if we allow the user to select a board size
+let appleIndex = 0;
 
 document.addEventListener("DOMContentLoaded", function () {
     document.addEventListener("keyup", control); //control is a function that defines keycodes
@@ -19,11 +20,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function makeGameBoard () {
     for (let i = 0; i < 20; i++) {
-        let gameBoardRow = document.createElement("tr");
+        const gameBoardRow = document.createElement("tr");
         gameBoard.appendChild(gameBoardRow);
         // gameBoardRow.className = "game-board-row"
         for (let j = 0; j < 20; j++) {
-        let gameBoardCell = document.createElement("td")
+        const gameBoardCell = document.createElement("td")
         gameBoardRow.appendChild(gameBoardCell);
         gameBoardCell.className = "game-board-cell"
         }
@@ -31,7 +32,7 @@ function makeGameBoard () {
 }
 
 function startGame () {
-    let gameBoardCell = document.getElementsByClassName("game-board-cell");
+    const gameBoardCell = document.getElementsByClassName("game-board-cell");
     generateRandomApple (gameBoardCell);
     direction = 1; //adding 1 is right
     scoreDisplay.innerHTML = score;
@@ -43,8 +44,8 @@ function startGame () {
 }
 
 function snakeMoveOrDie () {
-    let gameBoardCell = document.getElementsByClassName("game-board-cell");
-    let gameOutcomeMessage = document.getElementsByClassName("game-outcome-message");
+    const gameBoardCell = document.getElementsByClassName("game-board-cell");
+    const gameOutcomeMessage = document.getElementsByClassName("game-outcome-message");
     if (snakeHitChecker(gameBoardCell)) {
         gameOutcomeMessage.innerHTML = "GAME OVER"
         return resetInterval(interval); 
@@ -62,9 +63,9 @@ function moveSnake (gameBoardCell) {
 }
 
 function snakeHitChecker (gameBoardCell) {
-    let bottomRow = document.getElementById("game-board").lastChild;
+    const bottomRow = document.getElementById("game-board").lastChild;
     bottomRow.className = "bottom-row";
-    let topRow = document.getElementById("game-board").firstChild;
+    const topRow = document.getElementById("game-board").firstChild;
     topRow.className = "top-row";
     if (gameBoardCell[snake[0] + direction].classList.contains("snake") || snake[0] === bottomRow && direction === width || snake[0] === topRow && direction === -width) { //still need right and left and need to test if top and bottom even works, //first part is correct, snake eating itself
         return true;
@@ -77,9 +78,12 @@ function resetInterval () {
 
 }
 
-function generateRandomApple () {
-
-}
+function generateRandomApple (gameBoardCell) {
+    do {
+        appleIndex = Math.floor(Math.random() * gameBoardCell.length)
+    }  while (gameBoardCell[appleIndex].classList.contains("snake"));
+        gameBoardCell[appleIndex].classList.add("apple");
+  }
 
 function eatApple () {
 
