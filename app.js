1e34de38b8b79aee59510758926593888aebd5b7
Brittany <brittanylcallus@gmail.com>
2022-05-29 16:45:41 -0400

    revisions started - generateRandomApple

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 //Using How to Build a Snake Game In JavaScript from freeCodeCamp.org as a guide to help me through getting the game functioning. I am not just copying the work, I am working through all of its parts so that I have an understanding of what each part does. I am changing things when I am confident that I know of another way to achieve the same outcome. 
 
 const gameBoard = document.getElementById("game-board");
-const scoreDisplay = document.getElementsByClassName("score-display");
-let snake = [gameBoard[0]];
+const scoreDisplay = document.getElementsByClassName("score-display")[0];
+let snake;
 let score = 0;
 let intervalTime = 0; //turn this into a tick function
 let interval = 0;
 let direction = 1; //adding one means to the right
 let width = 20; //maybe change this if we allow the user to select a board size
-let appleIndex = 0;
+let apple = 0;
 
 document.addEventListener("DOMContentLoaded", function () {
     direction = 0;
@@ -16,7 +16,6 @@ document.addEventListener("DOMContentLoaded", function () {
     startButton.addEventListener("click", function() {
         direction = 1
     })
-
   });
 
 function makeGameBoard () {
@@ -28,18 +27,42 @@ function makeGameBoard () {
         gameBoardRow.appendChild(gameBoardCell);
         gameBoardCell.className = "game-board-cell"
         }
+        gameBoardRow.lastChild.classList += " test"
     }
+}
+
+document.addEventListener("keydown", function (event) {
+    const keyName = event.key 
+    if (keyName === "ArrowUp") {
+        direction = -width;
+    } else if (keyName === "ArrowDown") {
+        direction = + width;
+    } else if (keyName === "ArrowRight") {
+        direction = 1;
+    } else if (keyName === "ArrowLeft") {
+        direction = -1;
+    } 
+})
+
+  //what if apple trys to generate where snake already is?
+function generateRandomApple (gameBoardCell) {
+    apple = gameBoardCell[Math.floor(Math.random() * gameBoardCell.length)];
+    apple.classList.add("apple")
 }
 
 function startGame () {
     const gameBoardCell = document.getElementsByClassName("game-board-cell");
     generateRandomApple (gameBoardCell);
+    snake = [1, 0];
     direction = 1; 
     scoreDisplay.innerHTML = "Score: " + score;
+
+    //rewrite the forEach for adding snake class.
+    // for (let snakeCell of snake) {
+    //     snake.className.add("snake");
+    // }
     intervalTime = 1000;
-    snake = [2, 1, 0];
-    currentIndex = 0;
-    snake.forEach((index) => gameBoardCell[index].classList.add("snake"));
+    // snake.forEach((index) => gameBoardCell[index].classList.add("snake"));
     clearInterval(interval);
     interval = setInterval(snakeMoveOrDie, intervalTime);
 }
@@ -76,26 +99,6 @@ function snakeHitChecker (gameBoardCell) {
     }
 }
 
-function generateRandomApple (gameBoardCell) {
-    do {
-        appleIndex = Math.floor(Math.random() * gameBoardCell.length)
-    }  while (gameBoardCell[appleIndex].classList.contains("snake"));
-        gameBoardCell[appleIndex].classList.add("apple");
-  }
-
-document.addEventListener("keydown", function (event) {
-    const keyName = event.key 
-    if (keyName === "ArrowUp") {
-        direction = -width;
-    } else if (keyName === "ArrowDown") {
-        direction = + width;
-    } else if (keyName === "ArrowRight") {
-        direction = 1;
-    } else if (keyName === "ArrowLeft") {
-        direction = -1;
-    } 
-})
-
 function eatApple (gameBoardCell, tail) {
     let speed = .9;
 if (gameBoardCell[snake[0]].classList.contains("apple")) {
@@ -131,6 +134,5 @@ moveSnake();
 eatApple();
 
 //dont forget!!!
-//need to add a start button. game is currently starting automatically.
 //fix the score and inner HTML text
-
+//add instructions: right now game starts whehn right or down arrows are pressed or start is clicked
